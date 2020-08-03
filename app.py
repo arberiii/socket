bbe0ec5317f9d8d02f895e6ade62ed70209283cf
Arber Avdullahu <arber_919@live.com>
2020-08-03 23:40:48 +0200

    init

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def receive_message(message):
+    print('########: {}'.format(message))
+
+if __name__ == '__main__':
+    socketio.run(app)

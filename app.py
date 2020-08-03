195a5552dd0ed2d7d680fe35641c31c501477300
Arber Avdullahu <arber_919@live.com>
2020-08-04 01:00:31 +0200

    add

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO 
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
@@ -10,9 +10,28 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
+@socketio.on('message from user', namespace="/messages")
+def receive_message_from_user(message):
+    print('USER MESSAGE: {}'.format(message))
+    emit('from flask', message.upper(), broadcast=True)
+
+'''
 @socketio.on('message')
 def receive_message(message):
     print('########: {}'.format(message))
+    send('This is a message from Flask.')
+
+@socketio.on('custom event')
+def receive_custom_event(message):
+    print('THE CUSTOM MESSAGE IS: {}'.format(message['name']))
+    emit('from flask', {'extension' : 'Flask-SocketIO'}, json=True)
+
+'''
+
+@app.route('/orginate')
+def orginate():
+    socketio.emit("server originated", "something you can't relate client")
+    return "<h1>Sent!</h1"
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/script.js
+++ static/script.js
@@ -1,5 +1,42 @@
-var socket = io.connect('http://127.0.0.1:5000');
+$(document).ready(function() {
 
-socket.on('connect', function() {
-    socket.send('I am now connected!');
+    var socket = io.connect('http://127.0.0.1:5000');
+
+    var socket_message = io("http://127.0.0.1:5000/messages");
+
+    $('#send').on('click', function() {
+        var message = $('#message').val();
+
+        socket_message.emit('message from user', message);
+
+    });
+
+    socket.on('from flask', function(msg) {
+        alert(msg);
+    });
+
+    socket.on('server originated', function(msg){
+        alert(msg);
+    })
+
+    /*
+
+    socket.on('connect', function() {
+    
+        socket.send('I am now connected!');
+
+        socket.emit('custom event', {'name' : 'Anthony'});
+
+        socket.on('from flask', function(msg) {
+            alert(msg['extension']);
+        });
+
+        socket.on('message', function(msg) {
+            alert(msg);
+        });
+        
+    });
+
+    */
+
 });
